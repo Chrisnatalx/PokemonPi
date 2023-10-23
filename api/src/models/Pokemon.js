450c32ab6e9db1131d58ad2b4cd3622f53c26e21
Chrisnatalx <natale.christian.a@gmail.com>
2023-10-23 11:29:37 -0300

    Install dependencies, create pokemon and type models

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -2,11 +2,45 @@ const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define('pokemon', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
+	// defino el modelo
+	sequelize.define('pokemon', {
+		id: {
+			type: DataTypes.UUID,
+			defaultValue: DataTypes.UUIDV4,
+			allowNull: false,
+			primaryKey: true,
+		},
+		name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		image: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		life: {
+			type: DataTypes.NUMBER,
+			allowNull: false,
+		},
+		attack: {
+			type: DataTypes.NUMBER,
+			allowNull: false,
+		},
+		defence: {
+			type: DataTypes.NUMBER,
+			allowNull: false,
+		},
+		speed: {
+			type: DataTypes.NUMBER,
+			allowNull: false,
+		},
+		height: {
+			type: DataTypes.NUMBER,
+			allowNull: false,
+		},
+		weight: {
+			type: DataTypes.NUMBER,
+			allowNull: false,
+		},
+	});
 };
